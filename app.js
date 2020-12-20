0caa3f25710a72101f1ef1c9142b046077226d8e
LightDev1 <karumasanmy@gmail.com>
2020-12-20 14:49:00 +0400

    Сделал инициализацию проекта, а также добавил обработку регистрации и входа в приложение

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+
+const app = express();
+
+app.use('/api/auth', require('./routes/auth.routes'));
+
+const PORT = config.get('port') || 5000;
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        })
+        app.listen(PORT, () => { console.log(`Сервер был запущен на порте ${PORT}`); });
+    } catch (e) {
+        console.log('Server error', e);
+        process.exit(1);
+    }
+}
+
+start();
---
+++ modeles/User.js
@@ -0,0 +1,10 @@
+const { Schema, model, Types } = require('mongoose');
+
+const schema = new Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    posts: [{ type: Types.ObjectId, ref: 'Post' }],
+    comments: [{ type: Types.ObjectId, ref: 'Comment' }]
+});
+
+module.exports = model('User', schema);
